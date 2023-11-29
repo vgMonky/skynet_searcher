ab45a9eb7e7da387bab1122ac42ce01c6346b0f7
vgMonky <126716555+vgMonky@users.noreply.github.com>
2023-11-28 23:33:45 -0300

    skygpu.net/v2/search and thumbor img optimization
    json data from skygpu.net/v2/search converted to js objects.  Optimizing img with Thumbor-150x150 and adding them to img landingGallery in real time.
    !!! still not connected to text area and buttons !!!

--- main.js
+++ main.js
@@ -1,35 +1,26 @@
-
-
-
 /* algorithm selector */
-
-function activeModel(model,img){
+function modelSelector(model){
     const a = document.getElementById(model);
     const i = a.firstChild
     const all = document.querySelectorAll(".model")
-    console.log(all)
     a.addEventListener(
         "mouseover",
         (event) => {
-            i.src=img;
             i.style.opacity="1";
-            i.style.zIndex="2";
+            i.style.cursor="pointer";
         },
         false,
-      );
+    );
 
-      a.addEventListener(
+    a.addEventListener(
         "mouseout",
         (event) => {
-            i.src="img/Frame 118.png";
-            i.style.opacity="0.5";
-            i.style.zIndex="-1";
-            
+            i.style.opacity="0";
         },
         false,
-      );
+    );
 
-      a.addEventListener(
+    a.addEventListener(
         "click",
         (event) => {
             all[0].style.border= "none";
@@ -38,19 +29,79 @@ function activeModel(model,img){
             all[3].style.border= "none";
             all[4].style.border= "none";
             all[5].style.border= "none";
-            a.style.border= "2px solid orange"
+            a.style.border= "2px solid var(--Orange)"
         },
         false,
-      );
+    );
+}
 
+modelSelector("model1")
+modelSelector("model2")
+modelSelector("model3")
+modelSelector("model4")
+modelSelector("model5")
+modelSelector("model6")
+/* algorithm selector END */
+
+
+
+
+
+
+
+
+/*function to show img in landingGallery */
+function searchedImg(src){
+    const landingGallery = document.getElementById("landingGallery")
+
+    var img = document.createElement("img");
+    img.src = src
+    img.id = "imgSerch"
+    img.className = "col col-6 col-md-4 col-lg-3 col-xl-2"
+    
+    landingGallery.appendChild(img);
 }
 
 
-activeModel("model1","/img/stable.png")
-activeModel("model2","/img/midj.png")
-activeModel("model3","/img/vang.jpg")
-activeModel("model4","/img/anime.png")
-activeModel("model5","/img/ink.png")
-activeModel("model6","/img/robot.png")
+
+let inputSerch = "hd"
+
+/*catch data from skygpu serch api*/
+const apiUrl = 'https://testnet.skygpu.net/v2/skynet/search';
+
+const requestData = {
+  prompt: inputSerch,
+  size: 40,
+};
+
+fetch(apiUrl, {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(requestData),
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+
+    let count = data.length;
+    for (let i = 0; i < count; i++) {
+
+        /* add direct image url as .ipfs_urlImg to each object */
+        const ipfsLink = "https://ipfs.skygpu.net/ipfs/";
+        data[i].ipfs_urlImg = ipfsLink + data[i].ipfs_hash + "/image.png";
+
+        /* encode and add url as .encodedUrl */
+        data[i].encodedUrl = encodeURIComponent(data[i].ipfs_urlImg);
+        /* create and add thumbor 150x150 version as .thumborUrl150 */
+        data[i].thumborUrl150 = `https://thumbor.skygpu.net/unsafe/150x150/${data[i].encodedUrl}`
+        
+        /* show thumborUrl150 images in landing gallery */
+        searchedImg(data[i].thumborUrl150)
 
 
+    }
+  })
+  .catch(error => console.error('Error:', error));
