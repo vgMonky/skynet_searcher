7c79c9f874e67c8c6b57f08dfda2a1602b8ae73a
vgMonky <126716555+vgMonky@users.noreply.github.com>
2023-12-01 14:08:19 -0300

    static prompt added to gallery div

--- main.js
+++ main.js
@@ -74,64 +74,14 @@ modelSelector("model6")
 ////* GALLERY *////
 
 
-/*function to add img in LANDING GALLERY */
-function searchedImg(src){
-    const landingGallery = document.getElementById("landingGallery")
+/*set Search input by user    default:"pixel"*/
+let keyword = "pixel";
 
-    var img = document.createElement("img");
-    img.src = src;
-
-    img.id = "imgSerch";
-    img.className = "col col-4 col-md-3 col-xl-2 landing_img ";
-
-    landingGallery.appendChild(img);
-}
-
-/*function to check if img is png*/
-function isPngImage(src, timeout = 1000) { // Default timeout of 1000 milliseconds
-  return new Promise((resolve, reject) => {
-      const controller = new AbortController();
-      const signal = controller.signal;
-
-      // Set a timeout to abort the fetch
-      const timeoutId = setTimeout(() => controller.abort(), timeout);
-
-      fetch(src, { signal })
-          .then(response => {
-              clearTimeout(timeoutId); // Clear the timeout
-              if (!response.ok) throw new Error('Network response was not ok.');
-              return response.blob();
-          })
-          .then(blob => {
-              const reader = new FileReader();
-              reader.onloadend = function() {
-                  const arr = new Uint8Array(reader.result.slice(0, 8));
-                  const pngHeader = [137, 80, 78, 71, 13, 10, 26, 10];
-                  const isPng = arr.length === pngHeader.length && arr.every((value, index) => value === pngHeader[index]);
-                  resolve(isPng);
-              };
-              reader.readAsArrayBuffer(blob);
-          })
-          .catch(error => {
-              if (error.name === 'AbortError') {
-                  console.error('Fetch aborted due to timeout:', src);
-                  resolve(false); // Resolve with false if the request was aborted
-              } else {
-                  console.error('Fetch error:', error);
-                  resolve(false); // Resolve with false instead of rejecting to continue processing
-              }
-          });
-  });
-}
-
-/*set Search input by user*/
-let inputSearch = "pixel";
-
-/*catch and process data from skygpu search api*/
+/*catch data from skygpu search api*/
 const apiUrl = 'https://testnet.skygpu.net/v2/skynet/search';
 
 const requestData = {
-  prompt: inputSearch,
+  prompt: keyword,
   size: 200,
 };
 
@@ -147,7 +97,9 @@ fetch(apiUrl, {
 .then(data => {
   console.log(data);
 
-  async function updateData() {
+
+/*function to process and update data raw img links, and finaly create divs with the succsesfull ones */
+async function updateData() {
     const ipfsLink = "https://ipfs.skygpu.net/ipfs/";
     const batchSize = 100; // Number of links to process concurrently
 
@@ -169,7 +121,7 @@ fetch(apiUrl, {
                   }
       
                   if (!isImagePng) {
-                      console.error(`Image at index ${i + index} not found or not a PNG.`);
+                      /*console.error(`Image at index ${i + index} not found or not a PNG.`); */
                       resolve(null); // Resolve with null for skipped items
                       return;
                   }
@@ -177,11 +129,14 @@ fetch(apiUrl, {
                   item.ipfs_urlImg = url;
                   item.encodedUrl = encodeURIComponent(url);
                   item.thumborUrl150 = `https://thumbor.skygpu.net/unsafe/150x150/${item.encodedUrl}`;
-                  searchedImg(item.thumborUrl150);
-                  console.log(`${i + index} ${item.thumborUrl150} was added`);
+
+
+                  /*create Div's with data*/
+                  searchedImg(item.thumborUrl150, data[index].params.prompt);
+                  /*console.log(`${i + index} ${item.thumborUrl150} was added`); */
                   resolve(item); // Resolve with the item
               } catch (error) {
-                  console.error(`Error processing image at index ${i + index}:`, error);
+                  /*console.error(`Error processing image at index ${i + index}:`, error);*/
                   resolve(null); // Resolve with null in case of error
               }
           });
@@ -189,9 +144,72 @@ fetch(apiUrl, {
       
       await Promise.all(promises);
     }
-    console.log(data);
 }
   updateData();
 
 })
+
 .catch(error => console.error('Error:', error));
+
+
+
+
+/*function to check if img is png*/
+function isPngImage(src, timeout = 1000) { // Default timeout of 1000 milliseconds
+    return new Promise((resolve, reject) => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+  
+        // Set a timeout to abort the fetch
+        const timeoutId = setTimeout(() => controller.abort(), timeout);
+  
+        fetch(src, { signal })
+            .then(response => {
+                clearTimeout(timeoutId); // Clear the timeout
+                if (!response.ok) throw new Error('Network response was not ok.');
+                return response.blob();
+            })
+            .then(blob => {
+                const reader = new FileReader();
+                reader.onloadend = function() {
+                    const arr = new Uint8Array(reader.result.slice(0, 8));
+                    const pngHeader = [137, 80, 78, 71, 13, 10, 26, 10];
+                    const isPng = arr.length === pngHeader.length && arr.every((value, index) => value === pngHeader[index]);
+                    resolve(isPng);
+                };
+                reader.readAsArrayBuffer(blob);
+            })
+            .catch(error => {
+                if (error.name === 'AbortError') {
+                   /* console.error('Fetch aborted due to timeout:', src); */
+                    resolve(false); // Resolve with false if the request was aborted
+                } else {
+                    console.error('Fetch error:', error);
+                    resolve(false); // Resolve with false instead of rejecting to continue processing
+                }
+            });
+    });
+  }
+
+
+/*function to add img in LANDING GALLERY */
+function searchedImg(src, prompt){
+    const landingGallery = document.getElementById("landingGallery")
+
+    var imgCard = document.createElement("div");
+    imgCard.className ="col col-4 col-md-3 col-xl-2 gallery_card ";
+
+    var img = document.createElement("img");
+    img.src = src;
+    img.className = "col col-12 gallery_img ";
+
+    var prmt = document.createElement("p");
+    prmt.textContent = prompt;
+    prmt.className = "img_prpmt";
+
+
+    landingGallery.appendChild(imgCard);
+    imgCard.appendChild(img);
+    imgCard.appendChild(prmt);
+}
+
